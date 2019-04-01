0b2a9b7f3c67985c380a4e939c427acae094121e
Junfei Ma <junfei.ma@uvm.edu>
2019-03-31 21:04:38 -0400

    Connect to DataBase, Test DATABASE(VT)

--- top.php
+++ top.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>CS205 Sample Query</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Junfei Ma">
+        <meta name="description" content="CS205 Sample Query">
 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+        <![endif]-->
+
+        <link rel="stylesheet" href="css/base.css" type="text/css" media="screen">
+
+        <?php
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // inlcude all libraries. 
+        // 
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        print '<!-- begin including libraries -->';
+        
+        include 'lib/constants.php';
+
+        include LIB_PATH . '/Connect-With-Database.php';
+
+        print '<!-- libraries complete-->';
+        ?>	
+
+    </head>
+
+    <!-- **********************     Body section      ********************** -->
