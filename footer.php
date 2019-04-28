7e2905414f061bd08dd5738f4cfb1a395cbf5d8b
BradJewett <47016769+BradJewett@users.noreply.github.com>
2019-04-28 15:40:09 -0400

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-  
-    <p>Code for this site can be found at: <a href="https://github.com/BradJewett/CS205FinalProject">https://github.com/BradJewett/CS205FinalProject</a></p>
-</footer>
-<!-- @@@@@@@@@@@@@@@@@@@@ Ends Footer             @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
-</body>
-</html>
